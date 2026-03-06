09922f0ffcc11d5eeb178f351d9669e84abe2430
Prince Verma <165509727+Princepodwalverma@users.noreply.github.com>
2026-03-06 18:11:11 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
       <footer className="mb-0 text-center">
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
-            <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="https://sahibsingh.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Sahib Singh</a>
-            </p>
+            <p className="mb-3 mb-md-0" aria-label="Made with love by Prince Verma">
+  Made with <span role="img" aria-label="love">❤️</span> by Prince Verma
+</p>
             <a className="text-dark fs-4" href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
             </a>
