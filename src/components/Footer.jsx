e2f10e442b4f4349207e220bdb62e1f6987ef414
Prince Verma <princepodwalverma@gmail.com>
2026-03-23 11:39:13 +0530

    added Jenkins pipeline

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,16 @@ const Footer = () => {
       <footer className="mb-0 text-center">
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
+<<<<<<< HEAD
             <p className="mb-3 mb-md-0" aria-label="Made with love by Prince Verma">
   Made with <span role="img" aria-label="love">❤️</span> by Prince Verma
 </p>
+=======
+           <p className="mb-3 mb-md-0" aria-label="Made with love by Prince Verma">
+  Made with <span role="img" aria-label="love">❤️</span> by Prince Verma
+</p>
+              
+>>>>>>> 6464b7b (added Jenkins pipeline)
             <a className="text-dark fs-4" href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
             </a>
